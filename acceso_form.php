9c119a1a8c9b81e5330dcac53e5db7ee475a0992
Patricio Ahumada <p.ahumadapaez@gmail.com>
2021-10-03 22:32:52 -0300

    Pestaña de registro con estilos css

--- acceso_form.php
+++ acceso_form.php
@@ -3,16 +3,118 @@
 
 <head>
     <title>Registro</title>
-    <link rel="stylesheet" type="text/css" href="">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@300;400;600&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/estilos.css">
 </head>
 
 <body>
-    <div>
-        <h2>Seleccione Formulario de Registro</h2><hr>
+    <div id="registro">
+        <h1>Seleccione Formulario de Registro</h1>
+        <main>
+            <div id="registros-seleccion" class="contenedor">
+                <a href="formulario_propietario.php">
+                    <p> Registrarme como <br> <span>Propietario</span></p>
+                    <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg"
+                        xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="460.298px" height="460.297px"
+                        viewBox="0 0 460.298 460.297" style="enable-background:new 0 0 460.298 460.297;"
+                        xml:space="preserve">
+                        <g>
+                            <g>
+                                <path d="M230.149,120.939L65.986,256.274c0,0.191-0.048,0.472-0.144,0.855c-0.094,0.38-0.144,0.656-0.144,0.852v137.041
+			                    c0,4.948,1.809,9.236,5.426,12.847c3.616,3.613,7.898,5.431,12.847,5.431h109.63V303.664h73.097v109.64h109.629
+			                    c4.948,0,9.236-1.814,12.847-5.435c3.617-3.607,5.432-7.898,5.432-12.847V257.981c0-0.76-0.104-1.334-0.288-1.707L230.149,120.939
+			                    z" />
+                                <path d="M457.122,225.438L394.6,173.476V56.989c0-2.663-0.856-4.853-2.574-6.567c-1.704-1.712-3.894-2.568-6.563-2.568h-54.816
+			                    c-2.666,0-4.855,0.856-6.57,2.568c-1.711,1.714-2.566,3.905-2.566,6.567v55.673l-69.662-58.245
+			                    c-6.084-4.949-13.318-7.423-21.694-7.423c-8.375,0-15.608,2.474-21.698,7.423L3.172,225.438c-1.903,1.52-2.946,3.566-3.14,6.136
+			                    c-0.193,2.568,0.472,4.811,1.997,6.713l17.701,21.128c1.525,1.712,3.521,2.759,5.996,3.142c2.285,0.192,4.57-0.476,6.855-1.998
+			                    L230.149,95.817l197.57,164.741c1.526,1.328,3.521,1.991,5.996,1.991h0.858c2.471-0.376,4.463-1.43,5.996-3.138l17.703-21.125
+			                    c1.522-1.906,2.189-4.145,1.991-6.716C460.068,229.007,459.021,226.961,457.122,225.438z" />
+                            </g>
+                        </g>
+                        <g>
+                        </g>
+                        <g>
+                        </g>
+                        <g>
+                        </g>
+                        <g>
+                        </g>
+                        <g>
+                        </g>
+                        <g>
+                        </g>
+                        <g>
+                        </g>
+                        <g>
+                        </g>
+                        <g>
+                        </g>
+                        <g>
+                        </g>
+                        <g>
+                        </g>
+                        <g>
+                        </g>
+                        <g>
+                        </g>
+                        <g>
+                        </g>
+                        <g>
+                        </g>
+                    </svg>
+                </a>
 
-        <a href="formulario_propietario.php">Registrarme como Propietario</a><br><br>
-
-        <a href="formulario_gestor.php">Registrarme como Gestor Inmobiliario</a>
+                <a href="formulario_gestor.php">
+                    <p> Registrarme como <br> <span>Gestor Inmobiliario</span></p>
+                    <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg"
+                        xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 293.75 293.75"
+                        style="enable-background:new 0 0 293.75 293.75;" xml:space="preserve">
+                        <path
+                            d="M247.176,0.506c-0.915-0.342-1.862-0.487-2.801-0.487V0h-195v0.02c-0.939,0-1.887,0.145-2.801,0.487
+	                    C43.447,1.675,41.375,4.662,41.375,8v77.849c0,1.928,0.696,3.791,1.961,5.247l27.5,31.651c1.529,1.759,3.737,2.753,6.038,2.753
+	                    c0.307,0,0.615-0.018,0.924-0.053c2.624-0.305,4.929-1.886,6.158-4.224l20.21-38.438l33.396,36.398L90.96,234.247
+                    	c-1.206,2.978-0.514,6.388,1.758,8.66l48.5,48.5c1.563,1.562,3.609,2.343,5.657,2.343s4.095-0.781,5.657-2.343l48.5-48.5
+	                    c2.271-2.271,2.964-5.682,1.758-8.66l-46.602-115.064l33.396-36.398l20.21,38.438c1.23,2.338,3.534,3.919,6.158,4.224
+	                    c0.309,0.036,0.617,0.053,0.924,0.053c2.301,0,4.509-0.994,6.038-2.753l27.5-31.651c1.265-1.456,1.961-3.319,1.961-5.247V8
+	                    C252.375,4.662,250.303,1.675,247.176,0.506z M187.727,61h-81.705L66.924,16h159.902L187.727,61z M75.239,103.42L57.375,82.859
+	                    V29.405l35.374,40.713L75.239,103.42z M236.375,82.859l-17.864,20.561l-17.51-33.302l35.374-40.713V82.859z" />
+                        <g>
+                        </g>
+                        <g>
+                        </g>
+                        <g>
+                        </g>
+                        <g>
+                        </g>
+                        <g>
+                        </g>
+                        <g>
+                        </g>
+                        <g>
+                        </g>
+                        <g>
+                        </g>
+                        <g>
+                        </g>
+                        <g>
+                        </g>
+                        <g>
+                        </g>
+                        <g>
+                        </g>
+                        <g>
+                        </g>
+                        <g>
+                        </g>
+                        <g>
+                        </g>
+                    </svg>
+                </a>
+            </div>
+        </main>
     </div>
 </body>
 
